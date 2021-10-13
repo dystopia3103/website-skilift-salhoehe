a3f7ed3e4ebf0de3c2e498bdfd026417326072e8
dystopia3103 <schaerpe@gmail.com>
2021-10-13 21:37:07 +0200

    Deploying via https instead of ssh

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ npm run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'www.example.com' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:dystopia3103/website-skilift-salhoehe.git master:gh-pages
+git push -f https://github.com/dystopia3103/website-skilift-salhoehe.git master:gh-pages
 cd -
