7448f075f80047c882b1fd06cbe636d9add70bcd
Petra Schär <petra.schaer@asinfotrack.ch>
2022-02-06 16:47:22 +0100

    Updated images and status text

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,113 +1,119 @@
 <template>
-  <div class="contact">
-    <div class="hero is-light">
-      <div class="hero-body has-text-centered">
-        <h1 class="title">Infos</h1>
-        <p class="subtitle">Wie Sie uns finden & Öffnungszeiten</p>
-      </div>
-    </div>
-    <div class="container">
-      <div class="columns mt-4">
-        <div class="column">
-          <div class="card">
-            <header class="card-header">
+	<div class="contact">
+		<div class="hero is-light">
+			<div class="hero-body has-text-centered">
+				<h1 class="title">Infos</h1>
+				<p class="subtitle">Wie Sie uns finden & Öffnungszeiten</p>
+			</div>
+		</div>
+		<div class="container">
+			<div class="columns mt-4">
+				<div class="column">
+					<div class="card">
+						<header class="card-header">
                 <span class="card-header-icon">
                   <font-awesome-icon size="2x" icon="road" class="has-text-primary"></font-awesome-icon>
                 </span>
-              <p class="card-header-title subtitle">Anfahrt</p>
-            </header>
-            <article class="card-content">
-              <p>
-                <font-awesome-icon icon="bus" size="lg" class="has-text-primary mr-3"/>
-                Mit dem Bus Linie 2 der BBA / Bus Aarau in Richtung Barmelweid / Haltestelle Salhöhe
-              </p>
-              <br>
-              <p>
-                <font-awesome-icon icon="car" size="lg" class="has-text-primary mr-3"/>
-                Mit dem Auto über die Passtrasse Aarau - Erlinsbach - Salhöhe – Kienberg – Frick (Grosser Parkplatz auf
-                Passhöhe)
-              </p>
-              <br>
-              <p>Der Skilift und das Schiihüsli befindet sich dann ca. Mitte Parkplatz vis-à-vis im Wald.</p>
-            </article>
-            <div class="card-image">
-              <figure class="image is-3by2">
-                <img src="gallery/map.jpg" alt="Karte markiert">
-              </figure>
-            </div>
-          </div>
-        </div>
-        <div class="column">
-          <div class="card">
-            <header class="card-header">
+							<p class="card-header-title subtitle">Anfahrt</p>
+						</header>
+						<article class="card-content">
+							<p>
+								<font-awesome-icon icon="bus" size="lg" class="has-text-primary mr-3"/>
+								Mit dem Bus Linie 2 der BBA / Bus Aarau in Richtung Barmelweid / Haltestelle Salhöhe
+							</p>
+							<br>
+							<p>
+								<font-awesome-icon icon="car" size="lg" class="has-text-primary mr-3"/>
+								Mit dem Auto über die Passtrasse Aarau - Erlinsbach - Salhöhe – Kienberg – Frick
+								(Grosser Parkplatz auf
+								Passhöhe)
+							</p>
+							<br>
+							<p>Der Skilift und das Schiihüsli befindet sich dann ca. Mitte Parkplatz vis-à-vis im
+								Wald.</p>
+						</article>
+						<div class="card-image">
+							<figure class="image is-3by2">
+								<img src="gallery/map.jpg" alt="Karte markiert">
+							</figure>
+						</div>
+					</div>
+				</div>
+				<div class="column">
+					<div class="card">
+						<header class="card-header">
                 <span class="card-header-icon">
                   <font-awesome-icon size="2x" icon="calendar" class="has-text-primary"/>
                 </span>
-              <p class="card-header-title subtitle">Öffnungszeiten</p>
-            </header>
-            <article class="card-content">
-              <div class="table-container">
-                <table class="table is-fullwidth">
-                  <tr>
-                    <th>Mittwoch</th>
-                    <th>Freitag</th>
-                    <th>Samstag</th>
-                    <th>Sonntag</th>
-                  </tr>
-                  <tr>
-                    <td class="table-success">19- 22 Uhr<br><span class="has-text-primary">Nachtskifahren mit beleuchteter Piste</span>
-                    </td>
-                    <td class="table-success">19- 22 Uhr<br><span class="has-text-primary">Nachtskifahren mit beleuchteter Piste</span>
-                    </td>
-                    <td class="table-success">13 - 17 Uhr</td>
-                    <td class="table-success">13 - 17 Uhr</td>
-                  </tr>
-                </table>
-              </div>
-              <p>Die Öffnungszeiten gelten bei geöffnetem Skilift.</p>
-            </article>
-          </div>
-          <div class="card mt-5">
-            <header class="card-header">
+							<p class="card-header-title subtitle">Öffnungszeiten</p>
+						</header>
+						<article class="card-content">
+							<div class="table-container">
+								<table class="table is-fullwidth">
+									<tr>
+										<th>Mittwoch</th>
+										<th>Freitag</th>
+										<th>Samstag</th>
+										<th>Sonntag</th>
+									</tr>
+									<tr>
+										<td class="table-success">19- 22 Uhr<br><span class="has-text-primary">Nachtskifahren mit beleuchteter Piste</span>
+										</td>
+										<td class="table-success">19- 22 Uhr<br><span class="has-text-primary">Nachtskifahren mit beleuchteter Piste</span>
+										</td>
+										<td class="table-success">13 - 17 Uhr</td>
+										<td class="table-success">13 - 17 Uhr</td>
+									</tr>
+								</table>
+							</div>
+							<p>Die Öffnungszeiten gelten bei geöffnetem Skilift.</p>
+						</article>
+					</div>
+					<div class="card mt-5">
+						<header class="card-header">
                 <span class="card-header-icon">
                   <font-awesome-icon size="2x" icon="snowflake" class="has-text-primary"/>
                 </span>
-              <p class="card-header-title subtitle">Aktuelles</p>
-            </header>
-            <article class="card-content">
-              <p class="has-text-centered has-text-weight-semibold is-size-3">Der Skilift ist aktuell <strong
-                  class="has-text-danger">geschlossen</strong>.</p>
-              <p class="has-text-centered">Der Skilift ist aktuell geschlossen und wir geniessen die Sommerpause.<br>
-                Wir freuen uns, Sie im kommenden Winter bei uns begrüssen zu dürfen.</p>
-              <p class="has-text-right has-text-grey mt-5">Zuletzt aktualisiert am 01.03.2021</p>
-            </article>
-          </div>
-          <div class="card mt-5 mb-5">
-            <header class="card-header">
+							<p class="card-header-title subtitle">Aktuelles</p>
+						</header>
+						<article class="card-content">
+							<p class="has-text-centered has-text-weight-semibold is-size-3">Der Skilift ist aktuell
+								<strong
+									class="has-text-danger">geschlossen</strong>.</p>
+							<p class="has-text-centered">Leider liegt momentan noch nicht genügend Schnee, um den Skilift
+								in Betrieb zu nehmen. Wir stehen aber in den Startlöchern und freuen uns darauf, die Piste
+								für vergnügliche Schneetage zu präparieren, sobald dies möglich ist.
+							</p>
+							<p class="has-text-right has-text-grey mt-5">Zuletzt aktualisiert am 06.02.2022</p>
+						</article>
+					</div>
+					<div class="card mt-5 mb-5">
+						<header class="card-header">
               <span class="card-header-icon">
                   <font-awesome-icon size="2x" icon="phone" class="has-text-primary"/>
               </span>
-              <p class="card-header-title subtitle">Kontakt</p>
-            </header>
-            <article class="card-content">
-              <div class="table-container">
-                <table class="table is-fullwidth">
-                  <tr>
-                    <td class="table-success">E-Mail</td>
-                    <td class="table-success"><a href="mailto:salhoehe@gmail.com">salhoehe@gmail.com</a></td>
-                  </tr>
-                  <tr>
-                    <td class="table-success">Skilift-Info-Telefon</td>
-                    <td class="table-success"><a href="tel:+41796063114">079 606 31 14</a></td>
-                  </tr>
-                </table>
-              </div>
-            </article>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+							<p class="card-header-title subtitle">Kontakt</p>
+						</header>
+						<article class="card-content">
+							<div class="table-container">
+								<table class="table is-fullwidth">
+									<tr>
+										<td class="table-success">E-Mail</td>
+										<td class="table-success"><a
+											href="mailto:salhoehe@gmail.com">salhoehe@gmail.com</a></td>
+									</tr>
+									<tr>
+										<td class="table-success">Skilift-Info-Telefon</td>
+										<td class="table-success"><a href="tel:+41796063114">079 606 31 14</a></td>
+									</tr>
+								</table>
+							</div>
+						</article>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
@@ -115,7 +121,7 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 
 @Component({
-  components: {}
+	components: {}
 })
 export default class Contact extends Vue {
 
@@ -124,6 +130,6 @@ export default class Contact extends Vue {
 
 <style lang="scss" scoped>
 img {
-  object-fit: cover;
+	object-fit: cover;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,107 +1,125 @@
 <template>
-  <div class="home">
+	<div class="home">
 
-    <section class="hero is-light is-fullheight-with-navbar">
-      <div class="hero-body">
+		<section class="hero is-light is-fullheight-with-navbar">
+			<div class="hero-body">
 
-        <div class="container has-text-centered">
+				<div class="container has-text-centered">
 
-          <h1 class="title">Willkommen!</h1>
-          <p class="subtitle">Wir sind das schii-team saalhöchi</p>
+					<h1 class="title">Willkommen!</h1>
+					<p class="subtitle">Wir sind das schii-team saalhöchi</p>
 
-          <div class="columns">
-            <div class="column">
+					<div class="columns">
+						<div class="column">
 
-              <div class="card mt-5" style="height: 100%">
-                <div class="card-image">
-                  <figure class="image is-3by1">
-                    <img src="gallery/team.jpg" alt="Teamfoto">
-                  </figure>
-                </div>
-                <article class="card-content">
-                  <p class="has-text-weight-semibold is-size-4">Lernen Sie uns kennen...<font-awesome-icon icon="grin-wink" size="lg" class="ml-4" /></p>
-                  <b-button type="is-primary mt-5" tag="router-link" to="/ueber-uns">Über uns</b-button>
-                </article>
-              </div>
-            </div>
+							<div class="card mt-5" style="height: 100%">
+								<div class="card-image">
+									<figure class="image is-3by1">
+										<img src="gallery/team.jpg" alt="Teamfoto">
+									</figure>
+								</div>
+								<article class="card-content">
+									<p class="has-text-weight-semibold is-size-4">Lernen Sie uns kennen...
+										<font-awesome-icon icon="grin-wink" size="lg" class="ml-4"/>
+									</p>
+									<b-button type="is-primary mt-5" tag="router-link" to="/ueber-uns">Über uns
+									</b-button>
+								</article>
+							</div>
+						</div>
 
-            <div class="column">
-              <div class="card mt-5" style="height: 100%">
-                <div class="card-image">
-                  <figure class="image is-3by1">
-                    <img src="gallery/skiing1.jpg" alt="Teamfoto">
-                  </figure>
-                </div>
-                <article class="card-content">
-                  <p class="has-text-weight-semibold is-size-4">... oder planen Sie Ihren Besuch<font-awesome-icon icon="calendar-alt" size="lg" class="ml-4" /></p>
-                  <b-button type="is-primary mt-5" tag="router-link" to="/kontakt-und-anfahrt">Infos</b-button>
-                </article>
-              </div>
+						<div class="column">
+							<div class="card mt-5" style="height: 100%">
+								<div class="card-image">
+									<figure class="image is-3by1">
+										<img src="gallery/skiing3.jpg" alt="Wegweiser">
+									</figure>
+								</div>
+								<article class="card-content">
+									<p class="has-text-weight-semibold is-size-4">... oder planen Sie Ihren Besuch
+										<font-awesome-icon icon="calendar-alt" size="lg" class="ml-4"/>
+									</p>
+									<b-button type="is-primary mt-5" tag="router-link" to="/kontakt-und-anfahrt">Infos
+									</b-button>
+								</article>
+							</div>
 
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 
-    <section class="hero is-fullheight-with-navbar is-primary">
-      <div class="hero-body has-text-centered is-flex-direction-column is-justify-content-center">
-        <h1 class="title">Impressionen</h1>
-        <p class="subtitle">Unser Skilift im Betrieb</p>
-        <div class="card mt-5">
-          <div class="card-content">
-            <div class="container">
-              <b-carousel id="home-carousel"
-                          arrow="true"
-                          repeat="true"
-                          arrow-hover="true"
-                          indicator="false"
-                          icon-pack="fas"
-                          icon-prev="arrow-left"
-                          icon-next="arrow-right">
-                <b-carousel-item v-for="(item, i) in 6" :key="i">
-                  <b-image class="image" :src="getImgUrl(i)" />
-                </b-carousel-item>
-              </b-carousel>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+		<section class="hero is-fullheight-with-navbar is-primary">
+			<div class="hero-body has-text-centered is-flex-direction-column is-justify-content-center">
+				<h1 class="title">Impressionen</h1>
+				<p class="subtitle">Unser Skilift im Betrieb</p>
+				<div class="card mt-5">
+					<div class="card-content">
+						<div class="container">
+							<b-carousel id="home-carousel" arrow="true" repeat="true" arrow-hover="true" indicator="false"
+								icon-pack="fas" icon-prev="arrow-left" icon-next="arrow-right">
+								<b-carousel-item v-for="(item, i) in 6" :key="i">
+									<b-image class="image" :src="getImgUrl(i)"/>
+								</b-carousel-item>
+							</b-carousel>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 
-  </div>
+	</div>
 </template>
 
 <script lang="ts">
 // @ is an alias to /src
-import Vue from "vue";
-import {Component} from "vue-property-decorator";
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
 
 @Component({
-  components: {}
+	components: {}
 })
 export default class Home extends Vue {
 
-  private getImgUrl(value: number): string|null {
-    const basePath = 'gallery/';
+	private getImgUrl(value: number): string | null {
+		const basePath = 'gallery/';
 
-    switch (value) {
-      case 0:
-        return basePath + 'skiing1.jpg';
-      case 1:
-        return basePath + 'machine.jpg';
-      case 2:
-        return basePath + 'skiing2.jpg';
-      case 3:
-        return basePath + 'night.jpg';
-      case 4:
-        return basePath + 'skiing3.jpg';
-      case 5:
-        return basePath + 'hutt.jpg';
-      default:
-        return null;
-    }
-  }
+		let image = '';
+		switch (value) {
+			case 0:
+				image = 'skiing1.jpg';
+				break;
+			case 1:
+				image = 'sign.jpg';
+				break;
+			case 2:
+				image = 'machine.jpg';
+				break;
+			case 3:
+				image = 'hutt2.jpg';
+				break;
+			case 4:
+				image = 'night.jpg';
+				break;
+			case 5:
+				image = 'skiing3.jpg';
+				break;
+			case 6:
+				image = 'hutt.jpg';
+				break;
+			case 7:
+				image = 'skiing2.jpg';
+				break;
+			case 8:
+				image = 'team2.jpg';
+				break;
+			default:
+				return null;
+		}
+
+		return basePath + image;
+	}
 
 }
 </script>
@@ -110,10 +128,17 @@ export default class Home extends Vue {
 @import "src/assets/scss/main.scss";
 
 #home-carousel, .carousel-items, .carousel, .carousel-item {
-  max-height: 60vh;
+	width: 100%;
+	height: 40vh;
 
-  figure {
-    max-height: 60vh;
-  }
+	@include from($desktop) {
+		height: 60vh;
+		width: 50vw;
+		max-width: 1200px;
+	}
+
+	image {
+		object-position: center;
+	}
 }
 </style>
